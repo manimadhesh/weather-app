8b90ec25a14ac3aee61e693a3a70aea5b6303e2d
Manikandan M <144896052+manimadhesh@users.noreply.github.com>
2024-10-23 12:57:00 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,21 +3,27 @@ const inputValue = document.querySelector(".inputValue");
 const name = document.querySelector(".name");
 const desc = document.querySelector(".desc");
 const temp = document.querySelector(".temp");
+const humidity = document.querySelector(".humidity");
 const display = document.querySelector(".display"); 
+const wind = document.querySelector(".wind"); 
 
 button.addEventListener("click", function () {
-fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputValue.value + '&units=metric&appid=60ec613bb09f2372e00e7efc4ddc7a50')
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputValue.value + '&units=metric&appid=60ec613bb09f2372e00e7efc4ddc7a50')
     .then(response => response.json())
-  .then(data => 
+    .then(data =>
+      // console.log(data)
     {
       var nameValue = data.name;
-      var descValue = data.weather[0].description;
-      var tempValue = `${data.main.temp}°C`;
+      var descValue = `( Desc : ${data.weather[0].description} )`;
+      var tempValue = `Temp : ${data.main.temp}°C`;
+      var humidityValue = `Humidity : ${data.main.humidity}%`;
+      var windValue= `Wind : ${data.wind.speed}km/hr`;
 
       name.innerHTML = nameValue;
       temp.innerHTML = tempValue;
-      desc.innerHTML = descValue;
-
+    desc.innerHTML = descValue;
+    humidity.innerHTML = humidityValue;
+     wind.innerHTML = windValue;
       
       display.style.display = "block";
   }
