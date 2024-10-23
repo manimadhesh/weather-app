d5e753074173c092481e2f58ee1fda00d175a113
Manikandan M <144896052+manimadhesh@users.noreply.github.com>
2024-10-23 13:44:10 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -7,26 +7,34 @@ const humidity = document.querySelector(".humidity");
 const display = document.querySelector(".display"); 
 const wind = document.querySelector(".wind"); 
 
-button.addEventListener("click", function () {
+
+function fetchWeather() {
   fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputValue.value + '&units=metric&appid=60ec613bb09f2372e00e7efc4ddc7a50')
     .then(response => response.json())
-    .then(data =>
-      // console.log(data)
-    {
+    .then(data => {
       var nameValue = data.name;
       var descValue = `( Desc : ${data.weather[0].description} )`;
       var tempValue = `Temp : ${data.main.temp}°C`;
       var humidityValue = `Humidity : ${data.main.humidity}%`;
-      var windValue= `Wind : ${data.wind.speed}km/hr`;
+      var windValue = `Wind : ${data.wind.speed}km/hr`;
 
       name.innerHTML = nameValue;
       temp.innerHTML = tempValue;
-    desc.innerHTML = descValue;
-    humidity.innerHTML = humidityValue;
-     wind.innerHTML = windValue;
-      
+      desc.innerHTML = descValue;
+      humidity.innerHTML = humidityValue;
+      wind.innerHTML = windValue;
+
       display.style.display = "block";
-  }
-)
+    })
     .catch(error => alert("Invalid city name"));
+}
+
+
+button.addEventListener("click", fetchWeather);
+
+//  this is one function running through the keyboard keys pressing 
+inputValue.addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+    fetchWeather(); 
+  }
 });
